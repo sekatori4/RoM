cb026b3df0c141e4db7b8572e97ce2d0d8b1e472
sekatori4 <puniktft8@gmail.com>
2023-11-02 22:08:55 +0300

    222

--- Assets/UI/ICONS/Scripts/BUTTON1_skelet.cs
+++ Assets/UI/ICONS/Scripts/BUTTON1_skelet.cs
@@ -14,10 +14,10 @@ public class BUTTON1_skelet : MonoBehaviour
     {
        
         Debug.Log("CLIIIIIIIIIIIICK");
-        
-       
-        GameObject skeletik =Instantiate(spawner_init.GetComponent<SPAWNER>().NPC1, spawner_init.GetComponent<SPAWNER>().spawnPoint1);
-       
+
+
+        GameObject skeletik = Instantiate(spawner_init.GetComponent<SPAWNER>().NPC1, spawner_init.GetComponent<SPAWNER>().spawnPoint1.transform);
+        // GameObject skeletik = Instantiate(spawner_init.GetComponent<SPAWNER>().NPC1, new Vector3(-12, 0, 0), Quaternion.identity);
 
         skeletik.transform.parent = null;
 
