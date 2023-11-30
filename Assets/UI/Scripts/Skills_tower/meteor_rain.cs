f22c419e79f16b0bc335ce43dc65a223b1c7fe32
sekatori4 <puniktft8@gmail.com>
2023-11-30 17:25:15 +0300

    cd_skills

--- Assets/UI/Scripts/Skills_tower/meteor_rain.cs
+++ Assets/UI/Scripts/Skills_tower/meteor_rain.cs
@@ -6,6 +6,7 @@ using UnityEngine.UI;
 
 public class meteor_rain : MonoBehaviour
 {
+    public Button selfbutton;
     private GameObject effect_to_dest;
     public GameObject meteor_rain_prefab;
     public Button knopka_dis;
@@ -13,23 +14,93 @@ public class meteor_rain : MonoBehaviour
     public float damage;
     public float kol_voln = 5f;
     float kol_voln_max;
-        // Start is called before the first frame update
+
+    public Image abilityImage_cd;
+    public Text abilityText_cd;
+    public float abilityCooldown = 18;
+
+    private bool isAbilityCooldown = false;
+
+    private bool klik_cd = false;
+    private float currentAbilityCooldown;
+    
+    // Start is called before the first frame update
     public void Start()
     {
         kol_voln_max = kol_voln;
         var mainPS = meteor_rain_prefab.GetComponent<ParticleSystem>().main;
         mainPS.duration = kol_voln;
-        
+
+        abilityImage_cd.fillAmount = 0;
+
+        abilityText_cd.text = "";
+
     }
 
     public void Update()
     {
-       
+        AbilityInput();
+
+        AbilityCooldown(ref currentAbilityCooldown, abilityCooldown, ref isAbilityCooldown, abilityImage_cd, abilityText_cd);
+
+    }
+
+    private void AbilityInput()
+    {
+        if (klik_cd == true && !isAbilityCooldown)
+        {
+            isAbilityCooldown=true;
+            currentAbilityCooldown = abilityCooldown;
+            klik_cd=false;
+            
+        }
+    }
+
+    private void AbilityCooldown(ref float currentCooldawn, float maxCooldown,ref bool isCooldown, Image skillImage, Text skillText)
+    {
+        if(isCooldown)
+        {
+            currentAbilityCooldown -= Time.deltaTime;
+
+            if (currentCooldawn <= 0f) 
+            {
+                isCooldown = false;
+                currentAbilityCooldown = 0f;
+                selfbutton.enabled = true;
+
+                if (skillImage != null)
+                {
+                    skillImage.fillAmount = 0f;
+                }
+                if(skillText != null)
+                {
+                    skillText.text = "";
+                }
+            }
+            else
+            {
+                if(skillImage != null)
+                {
+                    skillImage.fillAmount = currentCooldawn / maxCooldown;
+                }
+                if (skillText != null)
+                {
+                    skillText.text = Mathf.Ceil(currentCooldawn).ToString();
+                }
+
+            }
+
+        }
+
     }
 
 
     public void click_rain()
     {
+        klik_cd = true;     //--------------------кд скила
+
+        selfbutton.enabled = false;
+
         kol_voln_max = kol_voln;
 
         effect_to_dest = Instantiate(meteor_rain_prefab, new Vector3 (0,0,0), Quaternion.identity);
